9308d24292566fc1e983eb0d453d200b27091fe5
Yoyi <rodrigo.armijo.c@usach.cl>
2016-09-26 21:53:50 -0300

    Correccion fft en el eje frecuencia.

--- fft.py
+++ fft.py
@@ -3,18 +3,21 @@ import numpy as np
 from pylab import*
 from scipy.io import wavfile
 
+def obtenerGraficaFFT(frecuMues, señalAudio):
+ largSenal = len(señalAudio) #Se obtiene el largo maximo de la señal, N
+ transf = fft(señalAudio)#Se obtiene la transformada rapida
+ k = arange(largSenal) #Corresponden a los n valores de la TDF
+ T = largSenal/frecuMues #Se calcula el periodo
+ frq = k/T #Se obtiene la frecuencia para los k valores
+ rangoValoresPosi = int(ceil((largSenal+1)/2.0)) #La funcion fft de la biblioteca numpy define obtener hasta la mitad del largo, para obtener los valores positivos de la señal
+ frq = frq[0:rangoValoresPosi] #Se evaluan cada frecuencia, cada frec * n/N
+ transf = transf[0:rangoValoresPosi] #Se obtienen solo los valores positivos de la frecuencia
+ transf = abs(transf) #Se quitan los valores negativos de amplitud
+
+ plt.plot(frq, transf, color='b') #Se configuran los ejes y el color de la grafica
+ plt.xlabel('Frecuencia')
+ plt.ylabel('Amplitud')
+ plt.show()
+
 frecuMues, audio = wavfile.read('resources/audio_files/nya.wav')
-print('La frecuencia de muestre es %d', frecuMues)
-largSenal = len(audio)
-transf = fft(audio)
-transf = transf[0:largSenal]
-transf = abs(transf)
-if largSenal % 2 > 0:
-        transf[1:len(transf)] = transf[1:len(transf)] * 2
-else:
-        transf[1:len(transf) - 1] = transf[1:len(transf) - 1] * 2
-nPuntos = np.arange(largSenal)
-plt.plot(nPuntos, transf, color='k')
-plt.xlabel('Frecuencia')
-plt.ylabel('Amplitud')
-plt.show()
+obtenerGraficaFFT(frecuMues, audio)
