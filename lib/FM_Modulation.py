d6f060c2289608d8e0895b73c62fd3ceb70e2dcf
Grybyus <david.calistro@usach.cl>
2016-10-10 23:28:10 -0300

    Update FM_Modulation.py

--- lib/FM_Modulation.py
+++ lib/FM_Modulation.py
@@ -2,5 +2,5 @@ from numpy import sin, cos , pi, linspace,arange
 import fft as ffT
 from scipy.io import wavfile
 
-spm = Ac*cos(3 * (pi/7)*t + 1 * integrate.quad(lambda x: audioData.any(), 0, t))
+spm = Ac*cos(3 * (pi/7)*t + 1 * integrate.quad(lambda x: audioData.any(), 0, t))#ERROR<- explorando posibilidades aun
 ffT.fftGraphic(sampleFreq, spm)
