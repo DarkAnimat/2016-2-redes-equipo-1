d7b575f2e349db797dbee885841a208b6b243b2d
Nialevolstel <nialevolstel@gmail.com>
2016-10-16 00:38:15 -0300

    Archivo codificacion.py actualizado

--- lib/codificacion.py
+++ lib/codificacion.py
@@ -5,7 +5,6 @@ from PIL import Image
 PATH_MAIN = os.path.normpath(os.getcwd())
 PATH_IMAGE_RESOURCES = os.path.join(PATH_MAIN, "resources", "image")
 
-
 def format_image_path(path):
     if not(os.path.isabs(path)):
         new_path = os.path.join(PATH_IMAGE_RESOURCES, path)
@@ -23,7 +22,8 @@ def image_to_bin(path):
         bin_arr.append(row_arr)
     return bin_arr
 
-def bin_to_image(bin_arr, path):
+
+def bin_to_image(bin_arr):
     img_arr = []
     for row in bin_arr:
         row_arr = []
@@ -31,10 +31,11 @@ def bin_to_image(bin_arr, path):
             row_arr.append(int(pixel, 2))
         img_arr.append(row_arr)
     img_arr = np.array(img_arr, dtype=np.int8)
-
     img = Image.fromarray(img_arr, mode="L")
-    img.save(format_image_path(path), 'PNG')
+    return img
 
+def save_image(image, path):
+    image.save(format_image_path(path), 'JPEG')
 
 # Especificaciones del Codigo Hamming (12,8):
 # Información: 8 bits
