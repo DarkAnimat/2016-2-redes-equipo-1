e7e6bbac1ca1c48d759a200bbfa48a23ef18d231
Ellinovfoster <dania.montanares@usach.cl>
2016-10-16 14:37:15 -0300

    Codificacion de imagen usando codigo hamming.

--- lib/codificacion.py
+++ lib/codificacion.py
@@ -11,6 +11,7 @@ def format_image_path(path):
         return new_path
     return path
 
+
 def image_to_bin(path):
     img = Image.open(format_image_path(path)).convert('L')
     img_arr = np.array(img)
@@ -38,5 +39,88 @@ def save_image(image, path):
     image.save(format_image_path(path), 'JPEG')
 
 # Especificaciones del Codigo Hamming (12,8):
-# Información: 8 bits
-# Paridad: 4 bits
+M = 8   # Información (M): 8 bits
+K = 4   # Paridad (K): 4 bits
+        # Para determinarlo: 2^K - 1 >= M + k
+
+def encode_hamming_word(word):
+
+    if (len(word) != M):  raise ValueError('The word length is invalid')
+
+    codeword  = list("x" * (M + K))
+    counter = 0
+
+    for i in range(0, len(codeword)):
+        if not(is_power2(i+1)):
+            codeword[i] = word[counter]
+            counter = counter + 1
+
+    for i in range(0, len(codeword)):
+        if (is_power2(i+1)):
+            codeword[i] = generate_parity_value(i+1, codeword)
+
+    return ''.join(codeword)
+
+
+def decode_hamming_word(word):
+    if (len(word) != (M + K)): raise ValueError('The word length is invalid')
+
+    decoded_word = list("x" * M)
+
+    counter = 0
+    for i in range(0, len(word)):
+        if not(is_power2(i+1)):
+            decoded_word[counter] = word[i]
+            counter = counter + 1
+
+    return ''.join(decoded_word)
+
+
+def is_hamming_word_valid(word):
+    if (len(word) != (M + K)): raise ValueError('The word length is invalid')
+
+    valid = True
+    for i in range(0, len(word)):
+        if (is_power2(i+1)):
+            if (word[i] != generate_parity_value(i+1, word)):
+                valid = False
+
+    return valid
+
+
+def generate_parity_value(position, word):
+    exponent = obtain_power2_exponent(position)
+    ret = ""
+    first_found = True
+    for i in range(0, len(word)):
+        aux = '{:01b}'.format(i+1).zfill(K)
+        if(aux[-(exponent+1)] == "1"):
+            if (first_found):
+                first_found = False
+            elif(ret == ""):
+                ret = word[i]
+            else:
+                ret = xor_function(ret, word[i])
+    return ret
+
+
+def is_power2(num):
+	return num != 0 and ((num & (num - 1)) == 0)
+
+
+def obtain_power2_exponent(num):
+    if (is_power2(num)):
+        counter = 0
+        while(num != 1):
+            num = num/2
+            counter = counter + 1
+        return counter
+    else:
+        return -1
+
+
+def xor_function(a, b):
+    if ((a == "0" and b == "0") or (a == "1" and b == "1")):
+        return "0"
+    else:
+        return "1"
