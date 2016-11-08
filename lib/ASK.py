63f35648b76302ab2242ed75f720ede89145946c
Nialevolstel <nialevolstel@gmail.com>
2016-11-07 23:35:38 -0300

    Modulación ASK actualizada. Adicionalmente se incluye un archivo de prueba en al carpeta Test

--- lib/ASK.py
+++ lib/ASK.py
@@ -1,30 +1,79 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from numpy import pi as PI
+from numpy import cos as COS
 
-def ASK_Modulation(code):
-	dim = 1000
+CARRIER_AMPLITUDE_0 = 0
+CARRIER_AMPLITUDE_1 = 5
+CARRIER_FREQUENCY = 5
+PULSE_FREQUENCY = 2
+PULSE_SAMPLING_POINTS = 100
+
+def ask_modulation(datastream):
+	modulation_signal = obtain_modulation_signal(datastream)
+	time_vector = obtain_time_vector(modulation_signal)
+	carrier_signal = obtain_carrier_signal(time_vector)
+	return obtain_modulated_signal(modulation_signal, carrier_signal)
+
+
+def set_carrier_amplitude_0(value):
+	global CARRIER_AMPLITUDE_0
+	CARRIER_AMPLITUDE_0 = value
+
+
+def set_carrier_amplitude_1(value):
+	global CARRIER_AMPLITUDE_1
+	CARRIER_AMPLITUDE_1 = value
+
+
+def set_carrier_frequency(value):
+	global CARRIER_FREQUENCY
+	CARRIER_FREQUENCY = value
+
+
+def set_pulse_frequency(value):
+	global PULSE_FREQUENCY
+	PULSE_FREQUENCY = value
+
+
+def set_pulse_sampling_points(value):
+	global PULSE_SAMPLING_POINTS
+	PULSE_SAMPLING_POINTS = value
+
+
+def obtain_modulation_signal(datastream):
 	Vx = []
-	largo = (len(code))
-	for i in range (largo):
-		f = np.ones(dim)
-		x = f * code[i]
-		Vx = np.concatenate((Vx,x))
+	for bit in datastream:
+		x = np.ones(PULSE_SAMPLING_POINTS) * int(bit)
+		Vx = np.concatenate((Vx, x))
+	return Vx
 
 
-	dim2 = len(Vx)
-	t = np.linspace(0,largo,dim2)
-	plt.subplot(3,1,1)
-	plt.plot(t,Vx)
-	frec = 5
-	plt.subplot(3,1,2)
-	Am = 2
-	w1 = Am * np.pi * frec * t
-	Signal = np.cos(w1)
-	plt.plot(t,Signal)
-	plt.subplot(3,1,3)
-	mult = (Vx*Signal)
-	plt.plot(t,mult)
-	plt.show()
+def obtain_time_vector(modulation_signal):
+	carrier_sampling_points = len(modulation_signal)
+	bits_quantity = carrier_sampling_points / PULSE_SAMPLING_POINTS
+	duration = bits_quantity * ( 1 / PULSE_FREQUENCY )
+	Vt = np.linspace(0, duration, carrier_sampling_points)
+	print("duration1 = ", duration)
 
-Enter = [0,	0,1,1,0,1,1,0,0,1,0,1,1,1,1,1,1,1] #Ejemplo de informacion a enviar
-ASK_Modulation(Enter)
+	return Vt
+
+
+def obtain_carrier_signal(time_vector):
+	return 	COS(2*PI*CARRIER_FREQUENCY*time_vector)
+
+
+def obtain_modulated_signal(modulation_signal, carrier_signal):
+	carrier_sampling_points = len(modulation_signal)
+	modulated_signal = []
+	signal_0 = np.ones(carrier_sampling_points) * (CARRIER_AMPLITUDE_0 * carrier_signal)
+	signal_1 = np.ones(carrier_sampling_points) * (CARRIER_AMPLITUDE_1 * carrier_signal)
+	for i in range(0, carrier_sampling_points):
+		if (modulation_signal[i] == 0):
+			modulated_signal.append(signal_0[i])
+		elif (modulation_signal[i] == 1):
+			modulated_signal.append(signal_1[i])
+	modulated_signal = np.array(modulated_signal)
+	return modulated_signal
+
+
+
---
+++ test/test_modulacion_ASK.py
@@ -0,0 +1,80 @@
+from lib import ASK
+import matplotlib.pyplot as plt
+
+# REAL DATA STREAM EXAMPLE
+datastream_example = "01010001000100010000000000000000100000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000000000000000000000100000100000010100001010000100100001101010100010001001101010101000101100001011000010100110101000001010010010101111010111110"
+datastream_example = datastream_example[0:20]
+
+modulation_signal = ASK.obtain_modulation_signal(datastream_example)    # Modulation signal
+duration = len(datastream_example) * (1 / ASK.PULSE_FREQUENCY)          # Signal duration in seconds
+time_vector = ASK.obtain_time_vector(modulation_signal)                 # Time vector
+carrier_signal = ASK.obtain_carrier_signal(time_vector)                 # Carrier signal
+
+###########################
+# OOK MODULATION
+###########################
+
+modulated_signal_1 = ASK.obtain_modulated_signal(modulation_signal, carrier_signal) # Modulated signal (OOK)
+
+# FIRST PLOT: modulation signal
+plt.subplot(3, 1, 1)
+plt.title("OOK modulation")
+
+plt.ylim(-0.1, 1.1)
+plt.ylabel("Amplitude")
+plt.xlim(0, duration)
+plt.xlabel("Duration (Seconds)")
+plt.plot(time_vector, modulation_signal)
+
+# SECOND PLOT: carrier signal
+plt.subplot(3, 1, 2)
+plt.ylabel("Amplitude")
+plt.xlim(0, duration)
+plt.xlabel("Duration (Seconds)")
+plt.plot(time_vector, carrier_signal)
+
+# THIRD PLOT: modulated signal
+plt.subplot(3, 1, 3)
+plt.ylabel("Amplitude")
+plt.xlim(0, duration)
+plt.xlabel("Duration (Seconds)")
+plt.plot(time_vector, modulated_signal_1)
+
+plt.show()
+
+
+
+###########################
+# ASK MODULATION
+###########################
+
+ASK.set_carrier_amplitude_0(5)
+ASK.set_carrier_amplitude_1(10)
+modulated_signal_2 = ASK.obtain_modulated_signal(modulation_signal, carrier_signal) # Modulated signal (OOK)
+
+
+# FIRST PLOT: modulation signal
+plt.subplot(3, 1, 1)
+plt.title("ASK modulation")
+
+plt.ylim(-0.1, 1.1)
+plt.ylabel("Amplitude")
+plt.xlim(0, duration)
+plt.xlabel("Duration (Seconds)")
+plt.plot(time_vector, modulation_signal)
+
+# SECOND PLOT: carrier signal
+plt.subplot(3, 1, 2)
+plt.ylabel("Amplitude")
+plt.xlim(0, duration)
+plt.xlabel("Duration (Seconds)")
+plt.plot(time_vector, carrier_signal)
+
+# THIRD PLOT: modulated signal
+plt.subplot(3, 1, 3)
+plt.ylabel("Amplitude")
+plt.xlim(0, duration)
+plt.xlabel("Duration (Seconds)")
+plt.plot(time_vector, modulated_signal_2)
+
+plt.show()
