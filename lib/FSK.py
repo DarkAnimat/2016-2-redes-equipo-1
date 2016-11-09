c9053804404654ee986ee9b6cae0850facf9f301
Nialevolstel <nialevolstel@gmail.com>
2016-11-09 01:04:08 -0300

    Archivo FSK actualizado

--- lib/FSK.py
+++ lib/FSK.py
@@ -1,69 +1,74 @@
 ##################
 ###### Import's ######
 ##################
-
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
-from pylab import*
-from sympy import *
-from scipy import signal
-
-##################
-###### Functions ######
-##################
-
-def fftGraphic(sampleFreq, audioData):
- dataLen = len(audioData) #Se obtiene el largo maximo de la señal, N
- transf = fft(audioData)#Se obtiene la transformada rapida
- k = arange(dataLen) #Corresponden a los n valores de la TDF
- T = dataLen / sampleFreq #Se calcula el periodo
- frq = k/T #Se obtiene la frecuencia para los k valores
- maxRange = int(ceil((dataLen+1)/2.0)) #La funcion fft de la biblioteca numpy define obtener hasta la mitad del largo, para obtener los valores positivos de la señal
- frq = frq[0:maxRange] #Se evaluan cada frecuencia, cada frec * n/N
- transf = transf[0:maxRange] #Se obtienen solo los valores positivos de la frecuencia
- transf = abs(transf) #Se quitan los valores negativos de amplitud
- plt.plot(frq, transf, color='b') #Se configuran los ejes y el color de la grafica
- plt.xlabel('Frecuencia')
- plt.ylabel('Amplitud')
- plt.show()
-
-def fskModulation(f1,f2, signal):
-    size = len(signal)
-    ySignal = []
-    dim = 500
-    for i in range(size):
-        f = np.ones(dim)
-        x = f * signal[i]
-        ySignal = np.concatenate((ySignal, x))
-    y = []
-    t = np.linspace(0,size,dim*size)
-    y1 = np.cos(2 * np.pi * f1 * t)
-    y2 = np.cos(2 * np.pi * f2 * t)
-    for i in range(len(ySignal)):
-        if (ySignal[i] == 0):
-            y.append(y1[i])
-        else:
-            y.append(y2[i])
-    fftGraphic(400, y)
-    plt.plot(t, y)
-    plt.show()
-    listReturn = [y,y1,y2]
-    return listReturn
-
-##################
-###### Main ######
-##################
-
-#v=[0,0,0,0,0,0,1,1,1,1,1,1] #Mensaje de prueba
-#v=[0,1,0,1,0,1,0,1,0,1,0,1] #Mensaje de prueba
-#v=[0,0,0,0,0,0,0,0,0,0] #Mensaje de prueba
-#v=[1,1,1,1,1,1,1,1,1,1] #Mensaje de prueba
+from numpy import pi as PI
+from numpy import cos as COS
 
 
-#print("El mensaje original es:")
-#print(v)  #Se imprime lo generado
+CARRIER_FREQUENCY_0 = 10
+CARRIER_FREQUENCY_1 = 20
+PULSE_FREQUENCY = 3
+PULSE_SAMPLING_POINTS = 500
 
-#list = fskModulation(5,20, v) #Se realiza la modulacion
+
+def bfsk_modulation(datastream):
+    modulation_signal = obtain_modulation_signal(datastream)
+    time_vector = obtain_time_vector(modulation_signal)
+    carrier_signal_0 = obtain_carrier_signal(CARRIER_FREQUENCY_0, time_vector)
+    carrier_signal_1 = obtain_carrier_signal(CARRIER_FREQUENCY_1, time_vector)
+    return obtain_modulated_signal(modulation_signal, carrier_signal_0, carrier_signal_1)
+
+
+def set_carrier_freq_0(value):
+    global CARRIER_FREQUENCY_0
+    CARRIER_FREQUENCY_0 = value
+
+
+def set_carrier_freq_1(value):
+    global CARRIER_FREQUENCY_1
+    CARRIER_FREQUENCY_1 = value
+
+
+def set_pulse_frequency(value):
+    global PULSE_FREQUENCY
+    PULSE_FREQUENCY = value
+
+
+def set_sampling_points(value):
+    global PULSE_SAMPLING_POINTS
+    PULSE_SAMPLING_POINTS = value
+
+
+def obtain_modulation_signal(datastream):
+    Vx = []
+    for bit in datastream:
+        x = np.ones(PULSE_SAMPLING_POINTS) * int(bit)
+        Vx = np.concatenate((Vx, x))
+    return Vx
+
+
+def obtain_time_vector(modulation_signal):
+	carrier_sampling_points = len(modulation_signal)
+	bits_quantity = carrier_sampling_points / PULSE_SAMPLING_POINTS
+	duration = bits_quantity * ( 1 / PULSE_FREQUENCY )
+	Vt = np.linspace(0, duration, carrier_sampling_points)
+	return Vt
+
+
+def obtain_carrier_signal(frequency, time_vector):
+    return COS(2 * PI * frequency * time_vector)
+
+
+def obtain_modulated_signal(modulation_signal, carrier_signal_0, carrier_signal_1):
+    carrier_sampling_points = len(modulation_signal)
+    modulated_signal = []
+    for i in range(0, carrier_sampling_points):
+        if modulation_signal[i] == 0:
+            modulated_signal.append(carrier_signal_0[i])
+        elif modulation_signal[i] == 1:
+            modulated_signal.append(carrier_signal_1[i])
+    modulated_signal = np.array(modulated_signal)
+    return modulated_signal
 
 
